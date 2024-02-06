ce4bbde7b1d05a356ce10814066fd2b4a25f7e04
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-02-06 16:07:28 +0300

    EfCar dal class

---
+++ DataAccess/Concrete/InEntityFramework/EfCarDal.cs
@@ -0,0 +1,69 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InEntityFramework
+{
+    internal class EfCarDal : ICarDal
+    {
+        public void Add(Car entity)
+        {
+            using (Rent_A_CarContext context = new Rent_A_CarContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+
+        }
+
+        public void Delete(Car entity)
+        {
+            using (Rent_A_CarContext context = new Rent_A_CarContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            using (Rent_A_CarContext context = new Rent_A_CarContext())
+            {
+                return context.Set<Car>().SingleOrDefault(filter);
+            }
+
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            using (Rent_A_CarContext context = new Rent_A_CarContext())
+            {
+                return filter == null
+                    ? context.Set<Car>().ToList()
+                    : context.Set<Car>().Where(filter).ToList();
+            }
+
+        }
+
+        public void Update(Car entity)
+        {
+            using (Rent_A_CarContext context = new Rent_A_CarContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    
+    }
+
+}
---
+++ DataAccess/Concrete/InEntityFramework/Rent_A_CarContext.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InEntityFramework
+{
+    internal class Rent_A_CarContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-SDDC56F\SQLEXPRESS;Database=Rent_A_Car;Trusted_Connection=true");
+        }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Colorr> Colors { get; set; }
+
+         
+    }
+
+}
