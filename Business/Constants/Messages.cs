9edba615e9f41bdab375bcfe4b51baf52606c525
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-02-10 19:39:15 +0300

    Creating Message class for constants

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+       public static string AddError = "The object couldn't add to the sytem.";
+
+        public static string AddSuccess = "The object have been added to the system.";
+        public static string DeleteError = "The object haven't been deleted from the system.";
+        public static string DeleteSuccess = "The object have been deleted from the system.";
+        public static string MaintenanceTime = "In these our the system in MaintenanceTime";
+        public static string IsNull = "The object is not exist";
+    }
+
+}
