cfd614aeca2d9fe848c08af90ea135380cfe2b87
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-02-11 21:58:47 +0300

    New Entities Customer, Rental, User

---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class User : IEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MyProperty { get; set; }
+        public int Password { get; set; }
+    }
+
+}
