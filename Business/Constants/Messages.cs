fbd95dd8a792139760a32bd173534a7e1c034706
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-02-11 22:00:47 +0300

    new messages

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -9,13 +9,15 @@ namespace Business.Constants
 {
     public static class Messages
     {
-       public static string AddError = "The object couldn't add to the sytem.";
-
+        public static string AddError = "The object couldn't add to the sytem."; 
         public static string AddSuccess = "The object have been added to the system.";
         public static string DeleteError = "The object haven't been deleted from the system.";
         public static string DeleteSuccess = "The object have been deleted from the system.";
         public static string MaintenanceTime = "In these our the system in MaintenanceTime";
         public static string IsNull = "The object is not exist";
+        public static string UpdateError = "The object couldn't Update.";
+        public static string UpdateSuccess = "The Object Updated successfully";
+        public static string ReturnCar = "The car has been returned";
     }
 
 }
