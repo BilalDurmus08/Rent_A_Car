d06a04572c3abea88e32e9d52d5ca0b1df2a03ba
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-02-11 22:01:24 +0300

    Some fixing error

--- Core/Utilites/Results/Concrete/ErrorResult.cs
+++ Core/Utilites/Results/Concrete/ErrorResult.cs
@@ -8,12 +8,12 @@ namespace Core.Utilites.Results.Concrete
 {
     public class ErrorResult : Result
     {
-        public ErrorResult(string message) : base(true, message) 
+        public ErrorResult(string message) : base(false, message) 
         {
             
         }
 
-        public ErrorResult() : base(true)
+        public ErrorResult() : base(false)
         {
             
         }
